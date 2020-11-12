4df509f17f02e63181695107407924fcfdf9d97c
Amanda <amanda@pette.net>
2020-11-12 12:50:37 -0500

    stage 3

--- Password Hacker/Password Hacker/task/hacking/hack.py
+++ Password Hacker/Password Hacker/task/hacking/hack.py
@@ -5,6 +5,7 @@ import itertools
 
 logging.basicConfig(level=logging.DEBUG, filename='log.log')
 
+
 def password_generator():
     chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     i = 1
@@ -14,6 +15,18 @@ def password_generator():
         for elem in password:
             yield ''.join(elem)
 
+
+def read_password_file():
+    with open('C:\\Users\\Amanda\\Google Drive\\python\\Password Hacker\\Password Hacker\\task\\hacking\\passwords.txt') as f:
+        word = f.readline().strip('\n')
+        while word:
+            password = itertools.product(*([char.upper(), char.lower()] for char in word))
+            for elem in password:
+                yield ''.join(elem)
+            word = f.readline().strip("\n")
+            logging.debug(f'new line {word}')
+
+
 def main():
     args = sys.argv
     with socket.socket() as client_socket:
@@ -22,7 +35,7 @@ def main():
         address = (hostname, port)
         client_socket.connect(address)
 
-        password = password_generator()
+        password = read_password_file()
         client_socket.send(next(password).encode())
 
         response = client_socket.recv(1024).decode()
@@ -32,5 +45,6 @@ def main():
             response = client_socket.recv(1024).decode()
             logging.debug(response + ', ' + pw)
         print(pw)
+
 
 main()
