4a4bc6ccb592fea4a0548293c83f6f15731d3a7c
Amanda <amanda@pette.net>
2020-11-14 14:52:18 -0500

    stage 7 - complete

--- Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
@@ -5,18 +5,41 @@ import sys
 
 logging.basicConfig(level=logging.DEBUG)
 
+available_languages = [
+        "arabic",
+        "german",
+        "english",
+        "spanish",
+        "french",
+        "hebrew",
+        "japanese",
+        "dutch",
+        "polish",
+        "portuguese",
+        "romanian",
+        "russian",
+        "turkish",
+    ]
+unavailable_languages = ["korean"]
 
 def get_URL(language_from, language_to, word):
-    url = f'https://context.reverso.net/translation/{language_from.lower()}-{language_to.lower()}/{word}'
+    url = f'https://context.reverso.net/translation/{language_from}-{language_to}/{word}'
     logging.debug(f"url: {url}")
     return url
 
-def check_HTTP_status(r):
-    if r.status_code == 200:
-        print("200 OK")
+
+def check_HTTP_status(request):
+    if request.status_code == 200:
         return True
     else:
+        print("Something wrong with your internet connection")
         return False
+
+
+def create_request(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"}
+    request = requests.get(url, headers=headers)
+    return request
 
 
 def choose_language():
@@ -61,8 +84,8 @@ def chose_word_to_translate():
     return word
 
 
-def translate_words(r):
-    soup = BeautifulSoup(r.content, 'html.parser')
+def translate_words(request):
+    soup = BeautifulSoup(request.content, 'html.parser')
     translations = soup \
         .find('div', id="translations-content") \
         .find_all('a', {"class": "translation"})
@@ -80,8 +103,9 @@ def print_translated_words(language_to, translated_words):
         all_translated_words += f"{word}\n"
     return all_translated_words
 
-def translate_sentences(r):
-    soup = BeautifulSoup(r.content, 'html.parser')
+
+def translate_sentences(request):
+    soup = BeautifulSoup(request.content, 'html.parser')
     translations = soup \
         .find('section', id="examples-content") \
         .find_all('span', {"class": "text"})
@@ -97,7 +121,6 @@ def print_translated_sentences(language_to, translated_sentences):
     all_translated_sentences = f'{language_to} Examples: \n'
     tracker = False
     for sentence in translated_sentences:
-        # print(sentence)
         all_translated_sentences += f'{sentence}\n'
         if tracker:
             all_translated_sentences += '\n'
@@ -106,29 +129,25 @@ def print_translated_sentences(language_to, translated_sentences):
 
 
 def translate_all_languages(language_from, word):
-    language_dict = {
-        1: "Arabic",
-        2: "German",
-        3: "English",
-        4: "Spanish",
-        5: "French",
-        6: "Hebrew",
-        7: "Japanese",
-        8: "Dutch",
-        9: "Polish",
-        10: "Portuguese",
-        11: "Romanian",
-        12: "Russian",
-        13: "Turkish",
-    }
-    language_dict = {k: v for k, v in language_dict.items() if v.lower() != language_from}
-    for language_to in language_dict.values():
+    remaining_languages = [x for x in available_languages if x != language_from]
+    for language_to in remaining_languages:
         url = get_URL(language_from, language_to, word)
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"}
-        r = requests.get(url, headers=headers)
+        request = create_request(url)
+        translated_words = translate_words(request)
+        translated_sentences = translate_sentences(request)
+        write_translation(word, language_to, translated_words, translated_sentences)
+
+
+def translate_one_language(language_from, language_to, word):
+    url = get_URL(language_from, language_to, word)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"}
+    r = requests.get(url, headers=headers)
+    if check_HTTP_status(r):
+        print("200 OK")
         translated_words = translate_words(r)
         translated_sentences = translate_sentences(r)
-        write_translation(word, language_to, translated_words, translated_sentences)
+        print_translated_words(language_to, translated_words)
+        print_translated_sentences(language_to, translated_sentences)
 
 
 def write_translation(word, language_to, translated_words, translated_sentences):
@@ -140,26 +159,34 @@ def write_translation(word, language_to, translated_words, translated_sentences)
         print(all_translated_words, file=f)
         print(all_translated_sentences, file=f)
 
-def main():
-    # language_from, language_to = choose_language()
-    # word = chose_word_to_translate()
-    # language_from, language_to, word = 'English', 'all', 'cheese'
-    args = sys.argv
-    language_from = args[1]
-    language_to = args[2]
-    word = args[3]
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0"}
 
+def verify_input():
+    args = sys.argv
+    language_from = args[1].lower()
+    language_to = args[2].lower()
+    word = args[3].lower()
+    if language_from in unavailable_languages:
+        print(f"Sorry, the program doesn't support {language_from}")
+        sys.exit()
+    elif language_to in unavailable_languages:
+        print(f"Sorry, the program doesn't support {language_to}")
+        sys.exit()
+    elif language_from not in available_languages:
+        print(f"Sorry, unable to find {language_from}")
+        sys.exit()
+    elif language_to not in available_languages:
+        print(f"Sorry, unable to find {language_to}")
+        sys.exit()
+    return language_from, language_to, word
+
+
+def main():
+    language_from, language_to, word = verify_input()
     if language_to != "all":
-        url = get_URL(language_from, language_to, word)
-        r = requests.get(url, headers=headers)
-        if check_HTTP_status(r):
-            translated_words = translate_words(r)
-            translated_sentences = translate_sentences(r)
-            print_translated_words(language_to, translated_words)
-            print_translated_sentences(language_to, translated_sentences)
+        translate_one_language(language_from, language_to, word)
     else:
         translate_all_languages(language_from, word)
+
 
 if __name__ == "__main__":
     main()
