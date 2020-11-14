f5685ffe3aa440077a38ac9f45ba3ef787546878
Amanda <amanda@pette.net>
2020-11-14 16:51:24 -0500

    stage 3

--- Smart Calculator/Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -5,10 +5,12 @@ def get_user_input():
     if user_input == '/exit':
         print("bye")
         sys.exit()
+    if user_input == '/help':
+        print("The program calculates the sum of numbers")
     else:
         input_list = user_input.split()
-    int_list = [int(x) for x in input_list]
-    return int_list
+        int_list = [int(x) for x in input_list]
+        return int_list
 
 def addition(iterable):
     return sum(iterable)
@@ -18,6 +20,8 @@ def main():
         input_list = get_user_input()
         if input_list:
             print(addition(input_list))
+        else:
+            pass
 
 if __name__ == "__main__":
     main()
