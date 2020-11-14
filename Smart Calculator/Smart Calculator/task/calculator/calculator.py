97a5086bca9d385e886952b80a87b6479b931abd
Amanda <amanda@pette.net>
2020-11-14 16:44:51 -0500

    stage 2

---
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -0,0 +1,23 @@
+import sys
+
+def get_user_input():
+    user_input = input()
+    if user_input == '/exit':
+        print("bye")
+        sys.exit()
+    else:
+        input_list = user_input.split()
+    int_list = [int(x) for x in input_list]
+    return int_list
+
+def addition(iterable):
+    return sum(iterable)
+
+def main():
+    while True:
+        input_list = get_user_input()
+        if input_list:
+            print(addition(input_list))
+
+if __name__ == "__main__":
+    main()
